ac4afb47828484b01336c4ccbc7873dae4833c7c
Bassim Shaikh (40142761) <40142761@uxbridge.ac.uk>
2020-10-15 10:47:33 +0100

    added FPController code

--- MazeGame/Assets/Scripts/FPController.cs
+++ MazeGame/Assets/Scripts/FPController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FPController : MonoBehaviour
 {
+
+    bool isGrounded = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,29 @@ public class FPController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W)) {
+            transform.Translate(new Vector3(0, 0, 2) * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.S)) {
+            transform.Translate(new Vector3(0, 0, -2) * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A)) {
+            transform.Translate(new Vector3(-2, 0, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(new Vector3(2, 0, 0) * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.Space) && isGrounded == true) {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+        }
+    }
+
+    void JumpCooldown(Collider targetCollision) { //coroutine function to destroy rockets after specified time
+        if (targetCollision.gameObject.layer.Equals("Ground")) {
+            isGrounded = true;
+        }
+        else {
+            isGrounded = false;
+        }
     }
 }
