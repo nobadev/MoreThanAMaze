8d57320519bcb36d4671ee27792179fe2b5f3cdb
bass23 <62807778+bass23@users.noreply.github.com>
2020-10-18 20:53:14 +0100

    added movement & jump for FPController
    added proper ground check

--- MazeGame/Assets/Scripts/FPController.cs
+++ MazeGame/Assets/Scripts/FPController.cs
@@ -1,42 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class FPController : MonoBehaviour
 {
+    /* TO DO
+     *  jumpimpulse needs to be affected by gravity
+     *  adding float - affected by gravity
+     */
 
-    bool isGrounded = true;
+    [SerializeField] private int movementSpeed;
+    [SerializeField] private int jumpImpulse;
+    [SerializeField] private int sprintSpeed;
+    private Rigidbody rb;
+    private bool isGrounded;
+    public float mouseSensitivity = 100f;
+    public Transform playerController;
+    float xRotation = 0f;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rb = GetComponent<Rigidbody>(); //assigns the rigidbody component of the Player to rb 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        float mouseHorizontal = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseVertical = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseVertical;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerController.Rotate(Vector3.up * mouseHorizontal);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true) {
+            Jump();
+        }
+    }
+    private void FixedUpdate() {
+        Movement();
+    }
+
+    private void Movement() {
         if(Input.GetKey(KeyCode.W)) {
-            transform.Translate(new Vector3(0, 0, 2) * Time.deltaTime);
+            transform.Translate(new Vector3(0, 0, movementSpeed) * Time.deltaTime);
         }
         if(Input.GetKey(KeyCode.S)) {
-            transform.Translate(new Vector3(0, 0, -2) * Time.deltaTime);
+            transform.Translate(new Vector3(0, 0, -movementSpeed) * Time.deltaTime);
         }
         if(Input.GetKey(KeyCode.A)) {
-            transform.Translate(new Vector3(-2, 0, 0) * Time.deltaTime);
+            transform.Translate(new Vector3(-movementSpeed, 0, 0) * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D)) {
-            transform.Translate(new Vector3(2, 0, 0) * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.Space) && isGrounded == true) {
-            GetComponent<Rigidbody>().AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+        transform.Translate(new Vector3(movementSpeed, 0, 0) * Time.deltaTime);
         }
     }
 
-    void JumpCooldown(Collider targetCollision) { //coroutine function to destroy rockets after specified time
-        if (targetCollision.gameObject.layer.Equals("Ground")) {
+    private void Jump() {
+        rb.AddForce(0, jumpImpulse, 0, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Ground") {
             isGrounded = true;
         }
-        else {
+    }
+    private void OnCollisionExit(Collision collision) {
+        if(collision.gameObject.tag == "Ground") {
             isGrounded = false;
         }
     }
